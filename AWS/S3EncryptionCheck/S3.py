8ea201579a4a7f24662421bd62414398150c0d88
Matt Connelly <gunnarstahl007@gmail.com>
2025-12-01 14:46:53 -0500

    Implement S3 bucket encryption check
    Added S3 bucket encryption check using boto3.

--- AWS/S3EncryptionCheck/S3.py
+++ AWS/S3EncryptionCheck/S3.py
@@ -1,1 +1,24 @@
+import boto3
+from botocore.exceptions import ClientError
 
+s3 = boto3.client("s3")
+
+# get all buckets
+response = s3.list_buckets()
+buckets = response.get("Buckets", [])
+
+if not buckets:
+    print("No S3 buckets found.")
+else:
+    for bucket in buckets:
+        name = bucket["Name"]
+
+        try:
+            # try to get encryption settings
+            s3.get_bucket_encryption(Bucket=name)
+            print(name, "- ENCRYPTED")
+        except ClientError as e:
+            code = e.response["Error"]["Code"]
+            if code == "ServerSideEncryptionConfigurationNotFoundError":
+                print(name, "- NOT ENCRYPTED")
+            else:
